339b001ff31957d76f5170acc9dec78e6857438a
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-17 21:47:39 +0530

    login flow for auth app

--- 03authsystem/app.js
+++ 03authsystem/app.js
@@ -16,40 +16,81 @@ app.get("/", (req,res) => {
 app.post("/register", async(req, res) => {
   const { firstname, lastname, email, password } = req.body;
 try {
-      if (!(email && password && firstname && lastname)) {
-        res.status(400).send("All fields are required");
-      }
+  if (!(email && password && firstname && lastname)) {
+    res.status(400).send("All fields are required");
+  }
 
-      const existingUser = await User.findOne({ email }); // PROMISE
+  const existingUser = await User.findOne({ email }); // PROMISE
 
-      if (existingUser) {
-        res.status(401).send("User already exists");
-      }
+  if (existingUser) {
+    res.status(401).send("User already exists");
+  }
 
-      const myEncPassword = await bcrypt.hash(password, 10);
+  const myEncPassword = await bcrypt.hash(password, 10);
 
-      const user = await User.create({
-        firstname,
-        lastname,
-        email: email.toLowerCase(),
-        password: myEncPassword,
-      });
+  const user = await User.create({
+    firstname,
+    lastname,
+    email: email.toLowerCase(),
+    password: myEncPassword,
+  });
 
-      //token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.SECRET_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
-      user.token = token;
-      // update or not
-      user.password = undefined;
-      res.status(201).json(user);
+  //token
+  const token = jwt.sign({ user_id: user._id, email }, process.env.SECRET_KEY, {
+    expiresIn: "2h",
+  });
+  user.token = token;
+  // update or not
+  user.password = undefined;
+  // send token or send just success yes and redirect - choice
+
+  res.status(201).json(user);
 } catch (error) {
     console.log(error)
 }
 });
+
+app.post("/login", async (req, res) => {
+    console.log("loginroute")
+    try {
+        const {email, password} = req.body
+       
+
+        if (!(email && password)) {
+          res.status(400).send("Field is missing");
+        }
+
+        const user = User.findOne({email})
+
+        // if(!user){
+        //     res.status(400).send("You are not registered in our app")
+        // }
+
+      if (!user && (await bcrypt.compare(password, user.password))) {
+        console.log(user.password);
+        const token = jwt.sign(
+          { user_id: user._id, email },
+          process.env.SECRET_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
+
+        user.token = token;
+        user.password = undefined;
+        res.status(200).json(user);
+      }
+      else{
+         res.status(400).send("email or password is incorrect"); 
+      }
+     
+
+
+       
+
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 module.exports = app
