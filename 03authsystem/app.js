eb11a6497b8e0f983eb1ac7545add02d37dde7dc
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-17 17:51:47 +0530

    adding user registration

--- 03authsystem/app.js
+++ 03authsystem/app.js
@@ -1,10 +1,25 @@
 require("dotenv").config();
 const express = require('express');
-const app = express();
 
+const app = express();
+app.use(express.json());
 
 app.get("/", (req,res) => {
     res.send("<>Hello from auth system</>")
-})
+});
+
+app.post("/register", (req, res) => {
+    const {firstname, lastname, email, password} = req.body
+ if (!(email && password && firstname && lastname)) {
+      res.status(400).send("All fields are required");
+    }
+
+    const existingUser =  User.findOne({ email }); // PROMISE
+
+    if (existingUser) {
+      res.status(401).send("User already exists");
+    }
+    
+});
 
 module.exports = app
