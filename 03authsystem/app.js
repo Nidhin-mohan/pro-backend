3c25c2c588ccb17226b6bb1b491373c2c7e2ff29
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-17 18:27:51 +0530

    databse connecion added

--- 03authsystem/app.js
+++ 03authsystem/app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require('./config/database').connect();
 const express = require('express');
 
 const app = express();
@@ -8,13 +9,13 @@ app.get("/", (req,res) => {
     res.send("<>Hello from auth system</>")
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async(req, res) => {
     const {firstname, lastname, email, password} = req.body
  if (!(email && password && firstname && lastname)) {
       res.status(400).send("All fields are required");
     }
 
-    const existingUser =  User.findOne({ email }); // PROMISE
+    const existingUser = await User.findOne({ email }); // PROMISE
 
     if (existingUser) {
       res.status(401).send("User already exists");
---
+++ 03authsystem/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const MONGODB_URL = process.env.MONGODB_URL
+mongoose.set("strictQuery", false);
+
+exports.connect = () => {
+  mongoose
+    .connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log(`DB CONNECTED SUCCESSFULLY`))
+    .catch((error) => {
+      console.log(`DB CONNECTION FAILED`);
+      console.log(error);
+      process.exit(1);
+    });
+};
