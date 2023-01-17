878515508d943c766a6bccbc51095ed15009f4b8
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-17 20:51:12 +0530

    register route in auth app

--- 03authsystem/app.js
+++ 03authsystem/app.js
@@ -1,37 +1,55 @@
 require("dotenv").config();
 require('./config/database').connect();
+const User = require("./model/user");
 const express = require('express');
 const bcrypt = require("bcryptjs")
+const jwt = require('jsonwebtoken') 
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (req,res) => {
     res.send("<>Hello from auth system</>")
+    console.log("responce for /")
 });
 
 app.post("/register", async(req, res) => {
-    const {firstname, lastname, email, password} = req.body
- if (!(email && password && firstname && lastname)) {
-      res.status(400).send("All fields are required");
-    }
+  const { firstname, lastname, email, password } = req.body;
+try {
+      if (!(email && password && firstname && lastname)) {
+        res.status(400).send("All fields are required");
+      }
 
-    const existingUser = await User.findOne({ email }); // PROMISE
+      const existingUser = await User.findOne({ email }); // PROMISE
 
-    if (existingUser) {
-      res.status(401).send("User already exists");
-    }
+      if (existingUser) {
+        res.status(401).send("User already exists");
+      }
 
-    const myEncPassword = await bcrypt.hash(password, 10);
+      const myEncPassword = await bcrypt.hash(password, 10);
 
-     const user = await User.create({
-       firstname,
-       lastname,
-       email: email.toLowerCase(),
-       password: myEncPassword,
-     });
+      const user = await User.create({
+        firstname,
+        lastname,
+        email: email.toLowerCase(),
+        password: myEncPassword,
+      });
 
-    
+      //token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+      user.token = token;
+      // update or not
+      user.password = undefined;
+      res.status(201).json(user);
+} catch (error) {
+    console.log(error)
+}
 });
 
 module.exports = app
