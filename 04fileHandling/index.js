7239052db652391c755ff53142c55bb2c4330340
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-20 19:37:31 +0530

    adding ejs templates

--- 04fileHandling/index.js
+++ 04fileHandling/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.set('view engine', "ejs")
+
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +16,14 @@ app.get("/myget", (req, res) => {
   console.log(req.body);
 
   res.send(req.body);
+});
+
+app.get("/mygetform", (req, res) => {
+ res.render("getform");
+});
+
+app.get("/mypostform", (req, res) => {
+ res.render("postform");
 });
 
 app.listen(4000, () => console.log(`Server is running at port 4000`))
