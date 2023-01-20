8e0e12306101355afb270c3b1f1a7dd738b65b7e
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-20 20:00:33 +0530

    handling images in files

--- 04fileHandling/index.js
+++ 04fileHandling/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const fileUpload = require("express-fileupload")
 const app = express();
 
 app.set('view engine', "ejs")
@@ -7,13 +7,17 @@ app.set('view engine', "ejs")
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir:"/tmp/"
+}));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello from LCO</h1>");
 });
 
 app.get("/myget", (req, res) => {
-  console.log(req.body);
+  console.log(req.query);
 
   res.send(req.body);
 });
@@ -23,7 +27,14 @@ app.get("/mygetform", (req, res) => {
 });
 
 app.get("/mypostform", (req, res) => {
+   
  res.render("postform");
 });
+
+app.post("/mypost", (req, res) => {
+     console.log(req.body);
+     console.log(req.files);
+     res.send(req.body)
+})
 
 app.listen(4000, () => console.log(`Server is running at port 4000`))
