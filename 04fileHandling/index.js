89de92e6b33313c6d8f5de455ac93266b66674d6
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-20 19:22:13 +0530

    adding costom middleware

--- 04fileHandling/index.js
+++ 04fileHandling/index.js
@@ -2,4 +2,18 @@ const express = require("express");
 
 const app = express();
 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from LCO</h1>");
+});
+
+app.get("/myget", (req, res) => {
+  console.log(req.body);
+
+  res.send(req.body);
+});
+
 app.listen(4000, () => console.log(`Server is running at port 4000`))
