55b7c7cd0a3f8ebd2b883e63aedd11226892f211
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-21 20:31:41 +0530

    password  encryption using mongoose prototype

--- tshirtstore/models/user.js
+++ tshirtstore/models/user.js
@@ -43,3 +43,14 @@ const userSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+//encrypt password before save - HOOKS
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+
+module.exports = mongoose.model("User", userSchema);
