e981e828ac683cf84570bf7a538b84af8c8cd5f4
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-21 12:51:05 +0530

    razorpay

---
+++ 05razorpay/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json)
+
+app.get("/", (req, res) => {
+    res.send("hi there");
+    console.log("hello there")
+});
+
+app.post("/order", async(req,res) => {
+    const amount = req.body.amount;
+
+  var instance = new Razorpay({
+    key_id: "",
+    key_secret: ""
+    // this needs to go in .env
+  });
+
+  var options = {
+    amount: amount * 100, // amount in the smallest currency unit
+    currency: "INR",
+    receipt: "order_rcptid_11"
+  };
+  //   instance.orders.create(options, function (err, order) {
+  //     console.log(order);
+  //   });
+
+  const myOrder = await instance.orders.create(options);
+
+  res.status(200).json({
+    success: true,
+    amount,
+    order: myOrder
+  });
+})
+
+app.listen(4000, () => console.log(`server is running at port 4000  `));
