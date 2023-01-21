f342cbf0504b52e21efae50f69fe5394aa56d456
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-21 15:18:40 +0530

    injecting swagger doc and middleware

--- tshirtstore/app.js
+++ tshirtstore/app.js
@@ -2,12 +2,37 @@ const express = require("express")
 require('dotenv').config()
 
 const app = express()
+const morgan = require('morgan')
+const cookieParser = require("morgan")
+const fileUpload = require('express-fileupload')
+
+//for swagger documentation
+const swaggerUi = require("swagger-ui-express")
+const YAML = require("yamljs")
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+
+
+//regular middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+//cookies and file  middleware
+app.use(cookieParser())
+app.use(fileUpload())
+
+
+//morgan middleware
+app.use(morgan("tiny"));
 
 //import all rooutes here
 const home = require('./routes/home')
 
 //router middleware
 app.use('/api/v1',home)
+
+
 
 
 module.exports = app
