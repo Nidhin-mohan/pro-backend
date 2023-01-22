c2df040a3563c4544bd573847a5a6bc83169ebd5
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-22 09:24:38 +0530

    added signup , cookie route

---
+++ tshirtstore/utils/cookieToken.js
@@ -0,0 +1,20 @@
+const cookieToken = (user, res) => {
+   
+    const token = user.getJwtToken();
+
+    const options = {
+      expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    };
+
+    res.status(200).cookie("token", token.option).json({
+      success: true,
+      token,
+      user,
+    });
+}
+
+
+
+
+module.exports = cookieToken
