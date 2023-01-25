251a089a1a21a3798a07c0377a18b17df4162486
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-25 14:08:42 +0530

    added route -get all products

--- tshirtstore/routes/product.js
+++ tshirtstore/routes/product.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const {addProduct} = require("../controllers/productController")
+const {addProduct, getAllProduct} = require("../controllers/productController")
 const { isLoggedIn, customRole } = require("../middlewares/user");
 
+//user routes
+router.route("/products").get(getAllProduct);
 
-router.route("/addproduct").post(isLoggedIn, addProduct);
+
+//admin
+router.route("/admin/product/add").post(isLoggedIn, customRole("admin"), addProduct);
+
+
+
+
 
 
 module.exports = router;
