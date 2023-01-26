7a10f3be6b77b348af02de3140163716b9bd841e
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-26 09:19:48 +0530

    added  product controller - addReview

--- tshirtstore/controllers/productController.js
+++ tshirtstore/controllers/productController.js
@@ -180,5 +180,52 @@ exports.adminDeleteOneProduct = BigPromise(async (req, res, next) => {
   });
 });
 
+exports.addReview = BigPromise(async (req, res, next) => {
+ 
+  const {rating, comment, productId} = req.body
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment
+  }
+
+  const product = await Product.findById(productId)
+
+  const AlreadyReview = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
 
 
+  if (AlreadyReview) {
+    product.reviews.forEach((review) => {
+      if (review.user.toString() === req.user._id.toString()) {
+        review.comment =comment
+        review.rating =rating
+      } 
+    })
+    
+  } else {
+    product.reviews.push(review)
+    product.numberOfReviews = product.reviews.length;
+  }
+
+  //adjust ratings
+
+   product.ratings =
+     product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+     product.reviews.length;
+
+     await product.save({validateBeforeSave: false})
+
+     res.status(200).json({
+      success: true
+     })
+
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
