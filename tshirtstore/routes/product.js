76ecfe9ee2577e8ccbf76933a18348b785fd721a
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-26 09:43:49 +0530

    added product routes - add,delete,get reviews

--- tshirtstore/routes/product.js
+++ tshirtstore/routes/product.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {addProduct, getAllProduct,  adminUpdateOneProduct, adminDeleteOneProduct, getOneProduct} = require("../controllers/productController")
+const {addProduct, getAllProduct,  adminUpdateOneProduct, adminDeleteOneProduct, getOneProduct, addReview, deleteReview, getOnlyReviewsForOneProduct} = require("../controllers/productController")
 const { isLoggedIn, customRole } = require("../middlewares/user");
 
 //user routes
 router.route("/products").get(getAllProduct);
 router.route("/product/:id").get(getOneProduct);
+router.route("/review").put(isLoggedIn, addReview);
+router.route("/review").delete(isLoggedIn, deleteReview);
+router.route("/reviews").get(isLoggedIn, getOnlyReviewsForOneProduct);
+
 
 
 //admin
