83d6956abef31f341723db34a068b1d8d567d62e
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-27 11:14:29 +0530

    added razorpay controller and all payment routes

--- tshirtstore/controllers/paymentController.js
+++ tshirtstore/controllers/paymentController.js
@@ -24,3 +24,30 @@ exports.captureStripePayment = BigPromise(async (req, res, next) => {
     
     });
 });
+
+
+exports.sendRazorpayKey = BigPromise(async (req, res, next) => {
+  res.status(200).json({
+    razorpaykey: process.env.RAZORPAY_API_KEY,
+  });
+});
+
+
+exports.captureRazorpayPayment = BigPromise(async (req, res, next) => {
+  let instance = new Razorpay({
+    key_id: process.env.RAZORPAY_API_KEY,
+    key_secret: process.env.RAZORPAY_SECRET,
+  });
+
+  let options = {
+    amount: req.body.amount, // amount in the smallest currency unit
+    currency: "INR",
+  };
+  const myOrder = await instance.orders.create(options);
+
+  res.status(200).json({
+    success: true,
+    amount: req.body.amount,
+    order: myOrder,
+  });
+});
