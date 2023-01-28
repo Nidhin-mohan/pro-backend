77a750c86df46eb058e85c1d59cd897af69d9711
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-28 10:50:58 +0530

    added getoneOrder route

--- tshirtstore/controllers/orderController.js
+++ tshirtstore/controllers/orderController.js
@@ -31,3 +31,22 @@ exports.createOrder = BigPromise(async (req, res, next) => {
 });
 
 
+exports.getOneOrder = BigPromise(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new CustomError("please check order id", 401));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+
+
+
--- tshirtstore/routes/order.js
+++ tshirtstore/routes/order.js
@@ -12,5 +12,9 @@ const { isLoggedIn, customRole } = require("../middlewares/user");
 
 
 router.route("/order/create").post(isLoggedIn, createOrder);
+router.route("/order/:id").get(isLoggedIn, getOneOrder);
+
+
+
 
 module.exports = router;
