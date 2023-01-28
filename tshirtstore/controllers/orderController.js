0ed0dc6b8c0c9b184354fc8560ce9f98567f8379
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-01-28 12:33:35 +0530

    add order routes - admin getallorders updateorders delete orders

--- tshirtstore/controllers/orderController.js
+++ tshirtstore/controllers/orderController.js
@@ -48,5 +48,69 @@ exports.getOneOrder = BigPromise(async (req, res, next) => {
 });
 
 
+exports.getLoggedInOrders = BigPromise(async (req, res, next) => {
+  const order = await Order.find({ user: req.user._id });
+
+  if (!order) {
+    return next(new CustomError("please check order id", 401));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+
+exports.admingetAllOrders = BigPromise(async (req, res, next) => {
+  const orders = await Order.find();
+
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+exports.adminUpdateOrder = BigPromise(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order.orderStatus === "Delivered") {
+    return next(new CustomError("Order is already marked for delivered", 401));
+  }
+
+  order.orderStatus = req.body.orderStatus;
+
+  order.orderItems.forEach(async (prod) => {
+    await updateProductStock(prod.product, prod.quantity);
+  });
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+exports.adminDeleteOrder = BigPromise(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  await order.remove();
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+async function updateProductStock(productId, quantity) {
+  const product = await Product.findById(productId);
+
+  product.stock = product.stock - quantity;
+
+  await product.save({ validateBeforeSave: false });
+}
+
+
+
 
 
--- tshirtstore/routes/order.js
+++ tshirtstore/routes/order.js
@@ -13,7 +13,17 @@ const { isLoggedIn, customRole } = require("../middlewares/user");
 
 router.route("/order/create").post(isLoggedIn, createOrder);
 router.route("/order/:id").get(isLoggedIn, getOneOrder);
+router.route("/myorder").get(isLoggedIn, getLoggedInOrders);
 
+
+//admin routes
+router
+  .route("/admin/orders")
+  .get(isLoggedIn, customRole("admin"), admingetAllOrders);
+router
+  .route("/admin/order/:id")
+  .put(isLoggedIn, customRole("admin"), adminUpdateOrder)
+  .delete(isLoggedIn, customRole("admin"), adminDeleteOrder);
 
 
 
