63889165eb9bdc621b7280c412cc2e59516c520e
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-02-13 12:00:25 +0530

    Added login route and page

--- 08lcopassport/index.js
+++ 08lcopassport/index.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const auth = require("./routes/auth");
+// const passportConfig = require("./passport/passport");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+
+mongoose.set("strictQuery", false);
+//connect to the DB
+mongoose.connect("mongodb://127.0.0.1:27017/passport", () =>
+  console.log("DB CONNECTED")
+);
+
 
 const app = express();
 
 
 
 app.set("view engine", "ejs");
-
+app.use("/auth", auth);
 
 app.get("/", (req, res) => {
   res.render("home");
