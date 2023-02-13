cdf3b5a71a9b670f1b3a1c0471a06e2bf73a8c03
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-02-13 13:23:02 +0530

    Changed passportjs version to 0.5

--- 08lcopassport/index.js
+++ 08lcopassport/index.js
@@ -6,13 +6,29 @@ const passportConfig = require("./passport/passport");
 const passport = require("passport");
 const cookieSession = require("cookie-session");
 
+const app = express();
+
 mongoose.set("strictQuery", false);
 //connect to the DB
 mongoose.connect("mongodb://127.0.0.1:27017/passport", () =>
   console.log("DB CONNECTED")
 );
 
-const app = express();
+
+
+
+app.use(
+  cookieSession({
+    maxAge: 3 * 24 * 60 * 60 * 1000,
+    keys: ["thisislcotokenkey"], // dotenv
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
 
 app.set("view engine", "ejs");
 app.use("/auth", auth);
